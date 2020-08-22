7bd048f77178d4cb8494066a8cf8a9a011cbf3d9
FabriceTchekou <63434998+FabriceTchekou@users.noreply.github.com>
2020-08-22 09:31:43 -0700

    Mongoose Population

--- nucampsiteServer/models/user.js
+++ nucampsiteServer/models/user.js
@@ -1,20 +1,22 @@
 const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    username: {
+    firstname: {
         type: String,
-        required: true,
-        unique: true
+        default: ''
     },
-    password: {
-        type: String,
-        required: true
+    lastname: {
+    type: String,
+        default: ''
     },
     admin: {
         type: Boolean,
         default: false
     }
 });
+
+userSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('User', userSchema);
