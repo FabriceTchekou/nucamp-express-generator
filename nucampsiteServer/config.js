c8790e4b7dfe41c523b7a79be5173b9817049406
FabriceTchekou <63434998+FabriceTchekou@users.noreply.github.com>
2020-08-28 23:19:58 -0700

    Passport Facebook

--- nucampsiteServer/config.js
+++ nucampsiteServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '239698997265430',
+        clientSecret: '16612560f2860222d7342ba8b3752466'
+    }
 }
